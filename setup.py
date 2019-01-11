7d2b247726961c2c942df566186840652f6f161b
treyshotz <mads.lundegaard@live.no>
2019-01-11 15:34:35 +0100

    Add files via upload
    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "",
+    version = "1.0",
+    description = "A HTTPBasic bruteforcer",
+    author = "Treyshotz",
+    author_email = "mads.lundegaard@live.no",
+    packages = [""],
+    install_requires = ["requests"]
+)
